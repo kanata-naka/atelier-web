ab800105ab3b8a2b0cecafce77401e325e0e609a
kanata-fabiko <faubynet@gmail.com>
2020-04-14 02:02:31 +0900

    feat: タグ一覧の開閉をアニメーション化

--- src/modules/gallery/components/TagsInfo.js
+++ src/modules/gallery/components/TagsInfo.js
@@ -1,16 +1,24 @@
 import { useState, useEffect, useCallback, useRef } from "react"
 import Link from "next/link"
+import { Transition } from "react-transition-group"
+
+const transitionClasses = {
+  entering: "slide-entering",
+  entered: "slide-entered",
+  exiting: "slide-exiting",
+  exited: "slide-exited"
+}
 
 export default ({ tagsInfo }) => {
   const [collasped, setCollasped] = useState(true)
+  const [height, setHeight] = useState(48)
+  const tagListRef = useRef(null)
 
   // トグルボタンを押下した際の処理
-  const handleClickToggleButton = useCallback(
-    e => {
-      setCollasped(!collasped)
-    },
-    [collasped]
-  )
+  const handleClickToggleButton = useCallback(() => {
+    setHeight(tagListRef.current.clientHeight)
+    setCollasped(!collasped)
+  }, [collasped])
 
   // タグの最大件数
   const maxCount = Object.keys(tagsInfo).reduce(
@@ -19,29 +27,45 @@ export default ({ tagsInfo }) => {
   )
 
   return (
-    <div className={`tags-info ${!collasped ? "expanded" : ""}`}>
-      <ul className="tag-list">
-        {Object.keys(tagsInfo).map((tag, index) => {
-          const rate = tagsInfo[tag] / maxCount
-          return (
-            <TagInfo key={index} tag={tag} count={tagsInfo[tag]} rate={rate} />
-          )
-        })}
-      </ul>
-      <div className="tags-info-foreground" />
-      <div className="tags-info-slide-button">
-        <a
-          href="#"
-          className="tags-info-slide-button__link"
-          onClick={handleClickToggleButton}>
-          {collasped ? (
-            <i className="fas fa-chevron-down"></i>
-          ) : (
-            <i className="fas fa-chevron-up"></i>
-          )}
-        </a>
-      </div>
-    </div>
+    <Transition in={!collasped} timeout={500}>
+      {state => {
+        const transitionStyle = {
+          entering: { height: `${height}px` },
+          entered: { height: `${height}px` },
+          exiting: { height: "48px" },
+          exited: { height: "48px" }
+        }
+        return (
+          <div
+            className={`tags-info ${transitionClasses[state] || ""}`}
+            style={transitionStyle[state]}>
+            <ul className="tag-list" ref={tagListRef}>
+              {Object.keys(tagsInfo).map((tag, index) => {
+                const rate = tagsInfo[tag] / maxCount
+                return (
+                  <TagInfo
+                    key={index}
+                    tag={tag}
+                    count={tagsInfo[tag]}
+                    rate={rate}
+                  />
+                )
+              })}
+            </ul>
+            <div className="tags-info-foreground" />
+            <div className="tags-info-slide-button">
+              <a
+                href="#"
+                className="tags-info-slide-button__link"
+                onClick={handleClickToggleButton}>
+                <i className="fas fa-chevron-down tags-info-slide-button__down"></i>
+                <i className="fas fa-chevron-up tags-info-slide-button__up"></i>
+              </a>
+            </div>
+          </div>
+        )
+      }}
+    </Transition>
   )
 }
 
