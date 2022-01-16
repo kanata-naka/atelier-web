76c26792081af8b8b3e0b3c211c717574f63d898
kanata-naka <faubynet@gmail.com>
2022-01-17 03:14:58 +0900

    refactor

--- src/utils/pageUtil.tsx
+++ src/utils/pageUtil.tsx
@@ -1,5 +1,13 @@
 import { Pagination } from "../types";
 
+export const createPagination = (
+  page: number,
+  perPage: number,
+  total: number
+): Pagination => {
+  return { page, perPage, total };
+};
+
 export const getItemsByPage = <T,>(
   items: T[],
   page: number,
@@ -8,12 +16,4 @@ export const getItemsByPage = <T,>(
   const offset = perPage * (page - 1);
   const itemsByPage = items.slice(offset, offset + perPage);
   return [...itemsByPage];
-};
-
-export const createPagination = (
-  page: number,
-  perPage: number,
-  total: number
-): Pagination => {
-  return { page, perPage, total };
 };
