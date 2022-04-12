e89cafa1dcb4a6139f7e87dba79dc1c80ac3b328
kanata-naka <faubynet@gmail.com>
2022-04-13 02:20:59 +0900

    chore: stylelintを導入

---
+++ .stylelintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: [
+    "stylelint-scss",
+    "stylelint-prettier",
+  ],
+  extends: [
+    "stylelint-config-recess-order",
+    "stylelint-config-recommended-scss",
+    "stylelint-config-prettier",
+  ],
+};
